5d28c81a7c7d42e481886330e28e777d329acab6
Shoji Eiki <eiki.s50311998@gmail.com>
2023-09-10 23:01:14 +0900

    戻すボタンの修正

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const CompleteTodos = ({todos, onClickBack}) => {
+    return (
+      <div className="complete-area">
+      <p className="title">完了のTodo</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+            <button onClick={() => onClickBack(index)}>戻す</button>
+          </div>
+          )
+        })}
+      </ul>
+    </div>
+    )
+}
